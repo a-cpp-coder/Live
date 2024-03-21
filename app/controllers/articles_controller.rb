08a7ec2cc94344d8c25f73705bf81bbda63e2742
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-21 19:34:37 +0700

    create in crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,31 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all # with s
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id]) # with no s
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new(title: "...", body: "...") // dummy data
+    @article = Article.new(article_params) # real one
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+
 end
