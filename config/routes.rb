566ee0d506345bac774dca5fbf3cfdf4c6d453ec
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-23 01:36:28 +0700

    addding route for nested resources comments and the controller for it, everything fits

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
+  # Defines the root path route ("/"), eg:  # root "posts#index"
   root "articles#index"
 
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
-  
-  # Whenever we have such a combination of routes, controller actions, and views that work together to perform CRUD operations on an entity, we call that entity a resource.
-  resources :articles # it's a ruby dsl(domain specific language), also in rails db, :articles is a symbol
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Whenever we have such a combination of routes, controller actions, and views that work together to perform CRUD operations on an entity, we call that entity a resource.
+  resources :articles do # it's a ruby dsl(domain specific language), also in rails db, :articles is a symbol
+    resources :comments
+  end
+
+
 end
