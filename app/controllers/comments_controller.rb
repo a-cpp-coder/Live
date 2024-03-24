d9153ac79e6a2ec5d7c587b73c37f078f80d73c5
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-25 03:18:26 +0700

    adding deleting comment function, and add restrict in model, on delete article, destroying all dependent comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,14 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article) # using the article_path(@article) helper. As we have already seen, this calls the show action of the ArticlesController which in turn renders the show.html.erb template.
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+  # The destroy action will find the article we are looking at, locate the comment within the @article.comments collection, and then remove it from the database and send us back to the show action for the article.
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
